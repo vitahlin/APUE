abc4631b36584348ba6f749740af5d10091d4f8e
Vitah <linw1225@gmail.com>
2019-09-17 15:16:07 +0800

    Doc：注释调整

--- src/lib/apue.h
+++ src/lib/apue.h
@@ -16,10 +16,11 @@ using std::cout;
 using std::endl;
 using std::string;
 
-// 错误打印函数封装，在log_err.cc
+// 错误打印函数封装，在log_err.cpp
 void LogErr(string str);
 void LogErrQuit(string str);
 
+// 信号相关，tellwait.cpp
 void TELL_WAIT();
 void TELL_CHILD(pid_t);
 void TELL_PARENT(pid_t);
